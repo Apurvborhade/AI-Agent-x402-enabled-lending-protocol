1c147f16ad7cc6f27a47d8ac1bb2f38bf51db807
Apurvborhade <apurvaborhadee@gmail.com>
2025-11-22 17:53:50 +0530

    CM Init

--- smart-contracts/src/CreditManager.sol
+++ smart-contracts/src/CreditManager.sol
@@ -4,4 +4,24 @@ pragma solidity ^0.8.0;
 import {LendingPool} from "./LendingPool.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract CreditManager {}
+contract CreditManager {
+    // Placeholder state variable
+    address public owner;
+    LendingPool public lendingPool;
+    IERC20 public stablecoin;
+
+    mapping(address => uint256) private s_creditScore;
+    mapping(address => uint256) public s_totalBorrowed;
+    mapping(address => uint256) public s_totalRepaid;
+    mapping(address => uint256) private s_lateRepayments;
+    address[] private s_users;
+
+    uint256 private constant INITIAL_CREDIT_SCORE = 800;
+    uint256 private constant LOAN_CREDIT_THRESHOLD = 700;
+
+    constructor(address lendingPoolAddress, address stablecoinAddress) {
+        owner = msg.sender;
+        lendingPool = LendingPool(lendingPoolAddress);
+        stablecoin = IERC20(stablecoinAddress);
+    }
+}
