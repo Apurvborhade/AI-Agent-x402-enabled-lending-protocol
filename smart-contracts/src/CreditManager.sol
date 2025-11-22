8fc37e1e71faaee1182c12cd83ce56140cd9b228
Apurvborhade <apurvaborhadee@gmail.com>
2025-11-22 17:50:33 +0530

    Initialize CreditManager and LendingPool

---
+++ smart-contracts/src/CreditManager.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LendingPool} from "./LendingPool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CreditManager {}
